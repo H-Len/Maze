a49e2866b2bd66094dd8b54210d1a7af50b21726
H-Len <8978475+H-Len@users.noreply.github.com>
2024-01-09 10:51:33 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from graphics import Window
+from maze import Maze
+# import time
+
+
+def main():
+    num_rows = 6
+    num_cols = 8
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
+
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win, 10)
+    # print(time.localtime())
+    win.wait_for_close()
+
+
+main()
